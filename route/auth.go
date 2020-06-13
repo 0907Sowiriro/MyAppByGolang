1b85be0e7002c21a30ebc3efeff2387b125e8b04
0907Sowiriro <sowiriro@gmail.com>
2020-06-13 14:41:09 +0900

    route先を分けた

---
+++ route/auth.go
@@ -0,0 +1,24 @@
+package route
+
+import "net/http"
+
+func authenticate (w http.ResponseWriter, r *http.Request) {
+
+}
+
+func signupAccount(w http.ResponseWriter, r *http.Request){
+
+}
+
+func signup(w http.ResponseWriter, r *http.Request){
+
+}
+
+func logout(w http.ResponseWriter, r *http.Request){
+
+}
+
+func login(w http.ResponseWriter, r *http.Request){
+
+}
+
---
+++ route/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"github.com/0907Sowiriro/MyAppByGolang/route"
+)
+
+func main() {
+
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static", http.StripPrefix("/static/", files))
+
+	mux.HandleFunc("/", Index)
+	mux.HandleFunc("/err", err)
+
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signup_account", signupAccount)
+	mux.HandleFunc("/authenticate", authenticate)
+
+
+	mux.HandleFunc("/movie/:id", Show)
+	mux.HandleFunc("/movie/create", Create)
+	mux.HandleFunc("/movie/:id/update", Update)
+	mux.HandleFunc("/movie/:id/delete", Delete)
+
+
+	server := &http.Server {
+		Addr: 			"127.0.0.1:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
